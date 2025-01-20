6c0bdce80b37f179c86d82d0b2d189e5d8756fe1
Lihan Yang <yanglihancyrus@gmail.com>
2025-01-20 19:03:41 +0000

    feat: added file input

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -1,15 +1,38 @@
 package wacc
 
 import parsley.{Success, Failure}
+import java.nio.file.{Files, Paths}
 
-def main(args: Array[String]): Unit = {
-    println("hello WACC!")
+import parser.parse
 
-    args.headOption match {
-        case Some(expr) => parser.parse(expr) match {
-            case Success(x) => println(s"$expr = $x")
-            case Failure(msg) => println(msg)
+object Main {
+    val exitStatusSuccess = 0
+    val exitStatusFailure = -1
+    val exitStatusSyntaxError = 100
+    val exitStatusSemanticError = 200
+
+    def main(args: Array[String]): Unit = {
+        println("hello WACC!")
+
+        val fileName = args.headOption match {
+            case Some(fn) => fn
+            case None => {
+                println("No argument given!\n" +
+                    "Example usage: compile my_code.wacc")
+                System.exit(exitStatusFailure)
+                ""
+            }
         }
-        case None => println("please enter an expression")
+        val sourceCode = try Files.readString(Paths.get(fileName)) catch {
+            case _ => {
+                println(s"Can't find or read source file at $fileName")
+                System.exit(exitStatusFailure)
+                ""
+            }
+        }
+        parse(sourceCode) match {
+            case Success(x) => println("Success.")
+            case Failure(x) => println("Failure.")
+        }
     }
 }
