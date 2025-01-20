f2aefb861ae971cd1601e573b805009e04b552ff
yw8123 <yw8123@ic.ac.uk>
2025-01-20 20:02:07 +0000

    feat: lexer descriptions done

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -1,16 +1,86 @@
 package wacc
 
 import parsley.Parsley
-import parsley.token.Lexer
+import parsley.token.*
 import parsley.token.descriptions.*
 
 object lexer {
     private val desc = LexicalDesc.plain.copy(
-        // your configuration goes here
+        spaceDesc = SpaceDesc.plain.copy(
+            lineCommentStart = "#"
+        ),
+        numericDesc = NumericDesc.plain,
+        nameDesc = NameDesc.plain.copy(
+            identifierStart = Basic(c => c == '_' || c.isLetter),
+            identifierLetter = Basic(c => c == '_' || c.isLetterOrDigit)
+        ),
+        symbolDesc = SymbolDesc.plain.copy(
+            hardKeywords = Set[String](
+                "null",
+                "int", 
+                "bool",
+                "char",
+                "string",
+                "pair",
+                "begin",
+                "end",
+                "is",
+                "skip",
+                "read",
+                "free",
+                "return",
+                "exit",
+                "println",
+                "if",
+                "then",
+                "else",
+                "fi",
+                "while",
+                "do",
+                "done",
+                "call", 
+                "fst", 
+                "snd"
+            ), 
+            hardOperators = Set[String](
+                "!", 
+                "-",
+                "len",
+                "ord",
+                "chr", 
+                "*", 
+                "/", 
+                "%",
+                "+",
+                ">", 
+                ">=", 
+                "<", 
+                "<=", 
+                "==", 
+                "!=", 
+                "&&", 
+                "||"
+            )
+        ),
+        textDesc = TextDesc.plain.copy(
+            escapeSequences = EscapeDesc.plain.copy(
+                literals = Set[Char](
+                    '0', 
+                    'b', 
+                    't', 
+                    'n', 
+                    'f', 
+                    'r',
+                    '\"', 
+                    '\'', 
+                    '\\'
+                )
+            )
+        )
     )
     private val lexer = Lexer(desc)
 
-    val integer = lexer.lexeme.integer.decimal
+    val integer = lexer.lexeme.natural.decimal
     val implicits = lexer.lexeme.symbol.implicits
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
 }
