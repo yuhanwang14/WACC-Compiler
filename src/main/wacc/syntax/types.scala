4b7d02dfd8b66aa57fa44df0482cc3bd53b1f9d5
Ethan <14w.1011@gmail.com>
2025-01-23 13:35:13 +0000

    feat: types parser

--- src/main/wacc/syntax/types.scala
+++ src/main/wacc/syntax/types.scala
@@ -14,10 +14,8 @@ object types {
     case class ArrayType(t: WACCType)(val pos: (Int, Int)) extends WACCType
     // Pair Type
     case object ErasedPairType extends WACCType
-    type BaseType = IntType.type  | BoolType.type | CharType.type | StringType.type 
-    type PairElemType = BaseType | ArrayType | ErasedPairType.type 
-    case class NonErasedPairType(t1: PairElemType, t2: PairElemType)(val pos: (Int, Int)) extends WACCType
+    case class NonErasedPairType(t1: WACCType, t2: WACCType)(val pos: (Int, Int)) extends WACCType
 
-    object ArrayType extends ParserBridgePos1[WACCType, WACCType]
-    object NonErasedPairType extends ParserBridgePos2[WACCType, WACCType, WACCType]
+    object ArrayType extends ParserBridgePos1[WACCType, ArrayType]
+    object NonErasedPairType extends ParserBridgePos2[WACCType, WACCType, NonErasedPairType]
 }
