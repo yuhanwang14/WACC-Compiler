49f92551bb5e2b77341d62cf09e38c1250edd715
Ethan <14w.1011@gmail.com>
2025-01-28 04:41:41 +0000

    wip: debug parser for statements(issue in delimiter to separate statemtns)

--- src/main/wacc/parsers/statements_parser.scala
+++ src/main/wacc/parsers/statements_parser.scala
@@ -7,15 +7,15 @@ import wacc.lexer.implicits.implicitSymbol, wacc.lexer.*
 import parsley.combinator.*
 import syntax.expressions.*
 import parsers.expressions_parser.*
-import parsley.expr.chain
 import parsley.Parsley
+import parsley.expr.*
 
 object statements_parser {
     lazy val program = Program("begin" ~> many(func), stmt <~ "end")
 
-    lazy val func = Func(waccType, Ident(ident), "(" ~> option(paramList) <~ ")", "is" ~> stmt <~ "end")
+    lazy val func = atomic(Func(waccType, Ident(ident), "(" ~> option(paramList) <~ ")", "is" ~> stmt <~ "end"))
 
-    val paramList: Parsley[ParamList] = ParamList(param, many("," ~> param))
+    lazy val paramList: Parsley[ParamList] = ParamList(param, many("," ~> param))
     lazy val param = Param(waccType, Ident(ident))
 
     val skipStmt = "skip" as Skip
@@ -30,11 +30,11 @@ object statements_parser {
     lazy val ifStmt = If("if" ~> expr, "then" ~> stmt, "else" ~> stmt <~ "fi")
     lazy val whileStmt = While("while" ~> expr, "do" ~> stmt <~ "done")
     lazy val beginStmt = Begin("begin" ~> stmt <~ "end")
-    lazy val delimiterStmt = chain.left1(stmt)(Delimiter from ";")
-
+    // lazy val delimiterStmt = chain.right1(stmt)(Delimiter from ";")
     lazy val stmt: Parsley[Stmt] = skipStmt | declareStmt | assignStmt  
     | readStmt | freeStmt | returnStmt | exitStmt | printStmt 
-    | printlnStmt | ifStmt | whileStmt | beginStmt | delimiterStmt
+    | printlnStmt | ifStmt | whileStmt | beginStmt 
+
     lazy val lValue: Parsley[LValue] = Ident(ident) | arrayElem | pairElem
     lazy val pairElem = PairElem(("fst" ~> lValue) | ("snd" ~> lValue))
     lazy val rValue = expr | arrayLiter | newPair | pairElem
