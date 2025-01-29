9b157e8d1edaddf87710f61ee2b75f2949b87ecd
Daniel Li <dl1723@ash15.doc.ic.ac.uk>
2025-01-29 16:38:59 +0000

    feat: program parser done

--- src/main/wacc/parsers/statements_parser.scala
+++ src/main/wacc/parsers/statements_parser.scala
@@ -9,6 +9,7 @@ import syntax.expressions.*
 import parsers.expressions_parser.*
 import parsley.Parsley
 import parsley.expr.*
+import parsley.debug._
 
 object statements_parser {
     lazy val program = Program("begin" ~> many(func), stmt <~ "end")
@@ -26,14 +27,16 @@ object statements_parser {
     val returnStmt = Return("return" ~> expr)
     val exitStmt = Exit("exit" ~> expr)
     val printStmt = Print("print" ~> expr)
-    val printlnStmt = Println( "println" ~> expr)
+    val printlnStmt = Println("println" ~> expr)
     lazy val ifStmt = If("if" ~> expr, "then" ~> stmt, "else" ~> stmt <~ "fi")
     lazy val whileStmt = While("while" ~> expr, "do" ~> stmt <~ "done")
     lazy val beginStmt = Begin("begin" ~> stmt <~ "end")
-    // lazy val delimiterStmt = chain.right1(stmt)(Delimiter from ";")
-    lazy val stmt: Parsley[Stmt] = skipStmt | declareStmt | assignStmt  
-    | readStmt | freeStmt | returnStmt | exitStmt | printStmt 
-    | printlnStmt | ifStmt | whileStmt | beginStmt 
+
+    val simpleStmt: Parsley[Stmt] = skipStmt | printlnStmt | printStmt
+    | declareStmt | assignStmt | readStmt | freeStmt 
+    | returnStmt | exitStmt | ifStmt | whileStmt | beginStmt
+    lazy val delimiterStmt = chain.right1(simpleStmt)(Delimiter from ";")
+    lazy val stmt: Parsley[Stmt] = delimiterStmt
 
     lazy val lValue: Parsley[LValue] = Ident(ident) | arrayElem | pairElem
     lazy val pairElem = PairElem(("fst" ~> lValue) | ("snd" ~> lValue))
