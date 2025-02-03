552d5254012db8bd68c167b204a2ca62687a0ec7
Ethan <14w.1011@gmail.com>
2025-02-03 16:32:45 +0000

    wip: type check

--- src/main/wacc/semantic_checkers/type_checker.scala
+++ src/main/wacc/semantic_checkers/type_checker.scala
@@ -3,146 +3,110 @@ package semantic_checkers
 import AST.types.*
 import AST.statements.*
 import AST.expressions.*
-import scala.util.control.Breaks.{break, breakable}
 import parsley.Result
 import parsley.Success
 
 object type_checker {
     val defaultPos: (Int, Int) = (-1, -1)
-    def compatible(t1: WACCType, t2: WACCType): Boolean = 
-        t1 == t2 || ((t1, t2) match {
-            case (UnknownType(), _) => false
-            case (_, UnknownType()) => false
-            case (ArrayType(CharType()), StringType()) => true
-            case (ArrayType(tt1), ArrayType(tt2)) => 
-                compatible(tt1, tt2) && 
-                tt1 != ArrayType(CharType()(defaultPos))(defaultPos) && 
-                tt2 != StringType()(defaultPos)
-            case (NonErasedPairType(t1_1, t1_2), NonErasedPairType(t2_1, t2_2)) => 
-                compatible(t1_1, t2_1) && compatible(t1_2, t2_2) && 
-                t1_1 != ArrayType(CharType()(defaultPos)) && t2_1 != StringType()(defaultPos) &&
-                t1_2 != ArrayType(CharType()(defaultPos)) && t2_2 != StringType()(defaultPos)
-            case (NonErasedPairType(_, _), ErasedPairType()) => true
-            case (ErasedPairType(), NonErasedPairType(_, _)) => true    
-            case (AnyType(), _) => true
-            case (_, AnyType()) => true
-            case _ => false
-        })
 
-    def commonAncestor(ts: List[Option[WACCType]]): Option[WACCType] = ts match {
-    case Nil => None
-    case headOpt :: tail =>
-        headOpt match {
-        case None => None 
-        case Some(head) =>
-            var resultType = head
-            var valid = true
-            breakable {
-                tail.foreach {
-                    case Some(t) =>
-                        if (compatible(t, resultType)) {
-                            resultType = t
-                        }
-                        else if (!compatible(resultType, t)) {
-                            valid = false
-                            break
-                        }
-                    case None => {
-                        valid = false
-                        break
-                    }
-                }
+    def invariant(t1: WACCType, t2: WACCType): Boolean = 
+        t1 != ArrayType(CharType()(t1.pos))(t1.pos) && t2 != StringType()(t2.pos)
+
+    def weakened(t1: WACCType, t2: WACCType): Boolean = (t1, t2) match
+            case (ArrayType(CharType()), StringType()) => true
+            case (NonErasedPairType(_, _), ErasedPairType()) => true
+            case (ErasedPairType(), NonErasedPairType(_, _)) => true
+            case _ => false
+
+    def compatible(t1: WACCType, t2: WACCType): Boolean = 
+        t1 == t2 || weakened(t1, t2) || t1 == AnyType() || t2 == AnyType()
+
+    def lca(a: WACCType, b: WACCType): Option[WACCType] = {
+        if (compatible(a, b)) Some(b)
+        else if (compatible(b, a)) Some(a)
+        else None
+    }
+
+    def commonAncestor(ts: List[Option[WACCType]]): Option[WACCType] = {
+        val definedTypes = ts.flatten
+        definedTypes match {
+            case Nil => None
+            case head :: tail =>
+            tail.foldLeft(Option(head)) { (currentAncestor, t) =>
+                currentAncestor.flatMap(a => lca(a, t))
             }
-            if (valid) Some(resultType) else None
         }
     }
 
-    def getType(rVal: RValue)(
-        implicit st: SymbolTable
-    ): Option[WACCType] = rVal match {
-        case (expr: Expr) => getType(expr)
-        case ArrayLiter(es) => commonAncestor(es.map(getType))
-        case NewPair(e1, e2) => {
-            val tyOpt1 = getType(e1)
-            val tyOpt2 = getType(e2)
-            tyOpt1 match {
-                case Some(ty1) => tyOpt2 match {
-                    case Some(ty2) => Some(NonErasedPairType(ty1, ty2)(defaultPos))
-                    case None => None
-                }
-                case None => None
-            }
-        }
-        case First(insideLVal) => getType(insideLVal) match {
-            case Some(NonErasedPairType(t, _)) => Some(t)
-            case Some(ErasedPairType()) => Some(UnknownType()(defaultPos))
-            case _ => None
-        }
-        case Second(insideLVal) => getType(insideLVal) match {
-            case Some(NonErasedPairType(_, t)) => Some(t)
-            case Some(ErasedPairType()) => Some(UnknownType()(defaultPos))
-            case _ => None
-        }
-        case Call(Ident(name), _) => st.lookupFunction(name) match {
-            case Some(funcSign) => Some(funcSign.returnType)
-            case None => None
-        }
-    } 
+    def getType(rVal: RValue)(implicit st: SymbolTable): Option[WACCType] = rVal match {
+        
+        case ArrayLiter(elements) =>
+            commonAncestor(elements.map(getType))
+        
+        case NewPair(e1, e2) =>
+            for {
+                t1 <- getType(e1)
+                t2 <- getType(e2)
+            } yield NonErasedPairType(t1, t2)(defaultPos)
+        
+        case Call(Ident(name), _) =>
+            st.lookupFunction(name).map(_.returnType)
+    }
 
     def getType(lVal: LValue)(
         implicit st: SymbolTable
     ): Option[WACCType] = lVal match {
-        case ArrayElem(id, _) => getType(id: Expr) match {
-            case Some(ArrayType(t)) => Some(t)
-            case _ => None
-        }
-        case Ident(name) => st.lookupSymbol(name)
-        case First(insideLVal) => getType(insideLVal) match {
-            case Some(NonErasedPairType(t, _)) => Some(t)
-            case Some(ErasedPairType()) => Some(UnknownType()(defaultPos))
-            case _ => None
-        }
-        case Second(insideLVal) => getType(insideLVal) match {
-            case Some(NonErasedPairType(_, t)) => Some(t)
-            case Some(ErasedPairType()) => Some(UnknownType()(defaultPos))
-            case _ => None
-        }
+        case First(insideLVal) =>
+            getType(insideLVal).collect {
+                case NonErasedPairType(t, _) => t
+                case ErasedPairType()        => UnknownType()(defaultPos)
+            }
+        
+        case Second(insideLVal) =>
+            getType(insideLVal).collect {
+                case NonErasedPairType(_, t) => t
+                case ErasedPairType()        => UnknownType()(defaultPos)
+            }
     }
 
-    def getType(expr: Expr)(
-        implicit st: SymbolTable
-    ): Option[WACCType] = expr match {
-        case IntLiter(x) => Some(IntType()(defaultPos))
-        case BoolLiter(x) => Some(BoolType()(defaultPos))
-        case CharLiter(c) => Some(CharType()(defaultPos))
-        case StrLiter(s) => Some(StringType()(defaultPos))
-        case PairLiter() => Some(NonErasedPairType(AnyType()(defaultPos), AnyType()(defaultPos))(defaultPos))
-        case Paren(e) => getType(e)
-        case Not(e) => Some(BoolType()(defaultPos))
-        case Negate(e) => Some(IntType()(defaultPos))
-        case Len(e) => Some(IntType()(defaultPos))
-        case Ord(e) => Some(IntType()(defaultPos))
-        case Chr(e) => Some(CharType()(defaultPos))
-        case Mul(e1, e2) => Some(IntType()(defaultPos))
-        case Div(e1, e2) => Some(IntType()(defaultPos))
-        case Mod(e1, e2) => Some(IntType()(defaultPos))
-        case Add(e1, e2) => Some(IntType()(defaultPos))
-        case Sub(e1, e2) => Some(IntType()(defaultPos))
-        case Less(e1, e2) => Some(BoolType()(defaultPos))
-        case LessEqual(e1, e2) => Some(BoolType()(defaultPos))
-        case Greater(e1, e2) => Some(BoolType()(defaultPos))
-        case GreaterEqual(e1, e2) => Some(BoolType()(defaultPos))
-        case Equal(e1, e2) => Some(BoolType()(defaultPos))
-        case NotEqual(e1, e2) => Some(BoolType()(defaultPos))
-        case And(e1, e2) => Some(BoolType()(defaultPos))
-        case Or(e1, e2) => Some(BoolType()(defaultPos))
-        case Ident(name) => st.lookupSymbol(name)
-        case ArrayElem(id, _) => getType(id: Expr) match {
-            case Some(ArrayType(t)) => Some(t)
-            case _ => None
-        }
+    def getType(expr: Expr)(implicit st: SymbolTable): Option[WACCType] = expr match {
+        // Literal cases
+        case IntLiter(_)  => Some(IntType()(defaultPos))
+        case BoolLiter(_) => Some(BoolType()(defaultPos))
+        case CharLiter(_) => Some(CharType()(defaultPos))
+        case StrLiter(_)  => Some(StringType()(defaultPos))
+        case PairLiter()  =>
+            Some(NonErasedPairType(AnyType()(defaultPos), AnyType()(defaultPos))(defaultPos))
+        
+        // Parentheses
+        case Paren(e) =>
+            getType(e)
+        
+        // Unary operators
+        case Not(_)   => Some(BoolType()(defaultPos))
+        case Negate(_) => Some(IntType()(defaultPos))
+        case Len(_)   => Some(IntType()(defaultPos))
+        case Ord(_)   => Some(IntType()(defaultPos))
+        case Chr(_)   => Some(CharType()(defaultPos))
+        
+        // Binary operators producing Int results
+        case Mul(_, _) | Div(_, _) | Mod(_, _) | Add(_, _) | Sub(_, _) =>
+            Some(IntType()(defaultPos))
+        
+        // Binary operators producing Bool results
+        case Less(_, _) | LessEqual(_, _) | Greater(_, _) | GreaterEqual(_, _) |
+            Equal(_, _) | NotEqual(_, _) | And(_, _) | Or(_, _) =>
+            Some(BoolType()(defaultPos))
+        
+        // Identifiers
+        case Ident(name) =>
+            st.lookupSymbol(name)
+        
+        // Array elements
+        case ArrayElem(id, _) =>
+            getType(id: Expr).collect { case ArrayType(t) => t }
     }
-    
+
     def verifyUnary(expr: UnaryOp)(
         implicit st: SymbolTable
     ): Result[Error, Expr] = expr match {
