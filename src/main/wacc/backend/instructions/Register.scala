02a2ebdfbbb7c4f949eb8e836cbb94f4e0ead1af
Ethan <14w.1011@gmail.com>
2025-02-11 16:19:19 +0000

    feat: registers abstractions

---
+++ src/main/wacc/backend/instructions/Register.scala
@@ -0,0 +1,37 @@
+package instructions
+
+sealed trait Register {
+  def number: Int
+}
+
+object Register {
+  val IP0: XRegister = XRegister(16)
+  val IP1: XRegister = XRegister(17)
+  val PR:  XRegister = XRegister(18)
+  
+  val FP:  XRegister = XRegister(29) 
+  val LR:  XRegister = XRegister(30)
+  val SP:  XRegister = XRegister(31) 
+}
+
+final case class XRegister(number: Int) extends Register {
+  def asW: WRegister = WRegister(number)
+
+  override def toString: String = number match {
+    case 29 => "fp"  
+    case 30 => "lr"  
+    case 31 => "sp" 
+    case _  => s"x$number"
+  }
+}
+
+final case class WRegister(number: Int) extends Register {
+  def asX: XRegister = XRegister(number)
+
+  override def toString: String = number match {
+    case 29 => "wfp" 
+    case 30 => "wlr" 
+    case 31 => "sp"  
+    case _  => s"w$number"
+  }
+}
