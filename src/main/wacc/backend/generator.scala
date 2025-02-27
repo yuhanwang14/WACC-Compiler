5c3a213cec93ca065c43222253d153830edebffa
Daniel Li <daniel.li23@imperial.ac.uk>
2025-02-27 17:15:42 +0000

    feat: generate the pop register code together with the push code

--- src/main/wacc/backend/generator.scala
+++ src/main/wacc/backend/generator.scala
@@ -6,9 +6,10 @@ import backend.RegisterAllocator
 import instructions.*
 import instructions.Register.*
 import scala.collection.mutable.ListBuffer
-import math.floorDiv
 
 object Generator {
+
+  private val localLabelCount: Int = 0
 
   def generate(prog: Program)(implicit
       symbolTable: SymbolTable,
@@ -54,33 +55,47 @@ object Generator {
     asmLine += s"wacc_${func.ti._2.name}:"
 
     asmLine += STP(FP, LR, SP, ImmVal(-16), PreIndex).toString()
-    // temporarily push all registers saved by Callee
-    asmLine += pushCalleeRegisters(10)
-    asmLine += MOVReg(FP, SP).toString()
     
+    // temporarily push all registers saved by Callee
+    val (pushCode, popCode) = pushAndPopCalleeRegisters()
+
+    asmLine += pushCode
+    asmLine += MOVReg(FP, SP).toString()
+
+    asmLine += popCode
+    asmLine += LDP(FP, LR, SP, ImmVal(16), PostIndex).toString()
+
   }
   
   /**
-   * Push all registers saved by Callee (x19-x28) that are about to use.
+   * Generate code to push and pop all registers saved by Callee (x19-x28) that are about to use.
    * Assume that `numReg` > 0
    */
-  private def pushCalleeRegisters(numReg: Int = 10): String = {
-      val codeLines: ListBuffer[Instruction] = ListBuffer()
-      val offset: Int = floorDiv(numReg + 1, 2) * 16
+  private def pushAndPopCalleeRegisters(numReg: Int = 10): (String, String) = {
 
-      if (numReg == 1)
-        STP(XRegister(19), XZR, SP, ImmVal(-16), PreIndex).toString()
-      else {
-        codeLines += STP(XRegister(19), XRegister(20), SP, ImmVal(-offset), PreIndex)
-        var pushedNum: Int = 2
-        while(pushedNum < numReg) {
-          val R1 = XRegister(19 + pushedNum)
-          val R2 = if (numReg - pushedNum > 1) then XRegister(20 + pushedNum) else XZR
-          codeLines += STP(R1, R2, SP, ImmVal(8 * pushedNum), Offset)
-          pushedNum = pushedNum + 2
-        }
-        codeLines.mkString("\n")
-      } 
+    val offset = math.floorDiv(numReg + 1, 2) * 16
+
+    if (numReg == 1) {
+      val push = STP(XRegister(19), XZR, SP, ImmVal(-offset), PreIndex).toString()
+      val pop  = LDP(XRegister(19), XZR, SP, ImmVal(offset), PostIndex).toString()
+      (push, pop)
+    } else {
+
+      val firstPush = STP(XRegister(19), XRegister(20), SP, ImmVal(-offset), PreIndex).toString()
+      val lastPop   = LDP(XRegister(19), XRegister(20), SP, ImmVal(offset), PostIndex).toString()
+
+      val pairedInstrs = (2 until numReg by 2).map { pushedNum =>
+        val r1 = XRegister(19 + pushedNum)
+        val r2 = if (pushedNum + 1 < numReg) XRegister(20 + pushedNum) else XZR
+        (
+          STP(r1, r2, SP, ImmVal(8 * pushedNum), Offset).toString(),
+          LDP(r1, r2, SP, ImmVal(8 * pushedNum), Offset).toString()
+        )
+      }
+      val pushCode = (firstPush +: pairedInstrs.map(_._1)).mkString("\n")
+      val popCode  = (pairedInstrs.map(_._2) :+ lastPop).mkString("\n")
+      (pushCode, popCode)
+    }
   }
 
 }
