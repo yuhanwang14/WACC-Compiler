15cd487ecc2f74328808fc8123b47019be394287
Lihan Yang <yanglihancyrus@gmail.com>
2025-03-03 16:25:06 +0000

    fix: cleared redundant address code

--- src/main/wacc/backend/instructions/Address.scala
+++ src/main/wacc/backend/instructions/Address.scala
@@ -1,10 +1,5 @@
 package instructions
 
-sealed trait AddressMode
-case object Offset extends AddressMode
-case object PreIndex extends AddressMode
-case object PostIndex extends AddressMode
-
 trait Operand
 
 case class ImmVal(value: Int) extends Operand {
@@ -18,14 +13,3 @@ case class Offset(reg: Register, offset: ImmVal) extends Address(f"[$reg, $offse
 case class PreIndex(reg: Register, offset: ImmVal) extends Address(f"[$reg, $offset]!")
 
 case class PostIndex(reg: Register, offset: ImmVal) extends Address(f"[$reg], $offset")
-
-case class _Address(
-    reg: Register,
-    indexMode: AddressMode = Offset,
-    immVal: ImmVal
-) {
-  override def toString: String = indexMode match
-    case Offset    => s"[${reg.toString}, ${immVal.toString}]"
-    case PreIndex  => s"[${reg.toString}, ${immVal.toString}]!"
-    case PostIndex => s"[${reg.toString}], ${immVal.toString}"
-}
