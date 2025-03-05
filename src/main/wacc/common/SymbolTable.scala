57410f986a3a9a0a54648a7daaf351ca981df20d
Lihan Yang <yanglihancyrus@gmail.com>
2025-03-05 21:28:20 +0000

    style: formatted Scope and SymbolTable

--- src/main/wacc/common/SymbolTable.scala
+++ src/main/wacc/common/SymbolTable.scala
@@ -7,7 +7,7 @@ import types.FunctionSignature
   *
   * The instance also stores the current scope.
   */
-class SymbolTable {
+class SymbolTable:
   val funcTable: Map[String, (FunctionSignature, FunctionScope)] = Map()
   val funcScopes: ArrayBuffer[FunctionScope] = ArrayBuffer()
   val globalScope: Scope = GlobalScope()
@@ -15,29 +15,22 @@ class SymbolTable {
 
   def returnType = currentScope.returnType
 
-  def isGlobalScope() = currentScope == globalScope
+  def isGlobalScope = currentScope == globalScope
 
   /** Enter a new nested scope. */
-  def enterScope(): Unit = {
-    currentScope = currentScope.addChild()
-  }
+  def enterScope: Unit = currentScope = currentScope.addChild()
 
-  def exitToGlobalScope(): Unit = {
-    currentScope.resetShadow()
-    currentScope = globalScope
-  }
+  def exitToGlobalScope: Unit = currentScope = globalScope
 
   /** Exit the current scope.
     *
     * @throws IllegalStateException
     *   if the current scope is the global scope or a function scope.
     */
-  def exitScope(): Unit = {
-    currentScope.resetShadow()
+  def exitScope: Unit =
     currentScope = currentScope.parent.getOrElse(
       throw IllegalStateException("Attempting to exit from a global / function scope.")
     )
-  }
 
   // Adds a symbol to the current scope
   def addSymbol(
@@ -49,11 +42,12 @@ class SymbolTable {
   // Looks up a symbol from innermost to outermost scope
   def lookupSymbol(identifier: String): Option[ast.WaccType] = currentScope(identifier)
 
-  def unshadow(identifier: String): Option[String] = currentScope.shadower.apply(identifier)
+  def unshadow(identifier: String): Option[String] = currentScope.shadower(identifier)
 
   def addFunction(f: ast.Func): Boolean =
-    if (funcTable.contains(f.ti(1).name)) { false }
-    else {
+    if (funcTable.contains(f.ti(1).name)) then
+      false
+    else
       // Deconstruct a `Func` instance
       val identifier: String = f.ti(1).name
       val returnType: ast.WaccType = f.ti(0)
@@ -71,7 +65,6 @@ class SymbolTable {
       funcScopes.addOne(fScope)
       funcTable.addOne(identifier, (fSignature, fScope))
       true
-    }
 
   def lookupFunction(identifer: String): Option[FunctionSignature] =
     funcTable.get(identifer).map(_(0))
@@ -81,8 +74,5 @@ class SymbolTable {
     * @throws NoSuchElementException
     *   if the identifier is not present in `funcTable`
     */
-  def enterFunctionScope(identifier: String): Unit = {
+  def enterFunctionScope(identifier: String): Unit =
     currentScope = funcTable.getOrElse(identifier, throw IllegalArgumentException())(1)
-  }
-
-}
